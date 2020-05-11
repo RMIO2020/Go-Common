26b65de2e2bc26064bd4f55b6774357985c2ff49
石姚嘉 <shiyaojia@rockminer.com>
2020-05-11 18:35:13 +0800

    登录

---
+++ db/sup-admin/V1/admin.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `sup_admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `username` varchar(50) NOT NULL COMMENT '登录名',
+  `nickname` varchar(50) NOT NULL DEFAULT '' COMMENT '用户昵称',
+  `password` varchar(50) NOT NULL COMMENT '密码',
+  `google_key` varchar(4) NOT NULL COMMENT 'Google key',
+  `phone` varchar(11) NOT NULL DEFAULT '' COMMENT '手机号',
+  `email` varchar(200) NOT NULL DEFAULT '' COMMENT '邮箱',
+  `avatar` varchar(300) NOT NULL DEFAULT '' COMMENT '头像',
+  `introduction` varchar(300) NOT NULL DEFAULT '' COMMENT '简介',
+  `status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '状态（0 停止1启动）',
+  `u_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新时间',
+  `last_login_time` datetime NOT NULL DEFAULT current_timestamp() COMMENT '上次登录时间',
+  `last_login_ip` varchar(50) NOT NULL DEFAULT '' COMMENT '最近登录IP',
+  `c_time` datetime NOT NULL COMMENT '注册时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `PHONE` (`phone`),
+  UNIQUE KEY `EMAIL` (`email`),
+  UNIQUE KEY `GKEY` (`google_key`),
+  KEY `PASSWORD` (`password`)
+) ENGINE=InnoDB AUTO_INCREMENT=100 COMMENT='管理账户表';
