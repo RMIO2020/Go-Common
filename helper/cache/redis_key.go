2e6fb9cb2a095ada8baa66b59fdd525c6eaf8dc5
wuyongzhan@rockminer.com <2268769732@qq.com>
2020-06-23 19:13:28 +0800

    [version] -- 增加下单倒计时redis的key

--- helper/cache/redis_key.go
+++ helper/cache/redis_key.go
@@ -1,8 +1,9 @@
 package cache
 
 const (
-	UserToken     = "login_token_"   // 用户-token
-	GoogleKey     = "GG:KEY:"        // 用户-token
-	Transaction   = "Tx:"            // 处理交易 Token
-	CachePayOrder = "CachePayOrder:" // 处理交易 Token
+	UserToken      = "login_token_"     // 用户-token
+	GoogleKey      = "GG:KEY:"          // 用户-token
+	Transaction    = "Tx:"              // 处理交易 Token
+	CachePayOrder  = "CachePayOrder:"   // 处理交易 Token
+	OrderCountDown = "ping:order:down:" //拼团倒计时
 )
