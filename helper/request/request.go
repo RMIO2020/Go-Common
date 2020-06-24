a0b8d4ed5d3b245ed415e3a0f95f1aba9fc2d925
石姚嘉 <shiyaojia@rockminer.com>
2020-06-24 19:28:22 +0800

    法币支付地址

---
+++ helper/request/request.go
@@ -0,0 +1,57 @@
+package request
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"sort"
+	"strings"
+)
+
+const (
+	POST = "POST"
+)
+
+type ReqParams map[string]string
+
+func SortParams(params ReqParams) string {
+	keys := make([]string, len(params))
+	i := 0
+	for k := range params {
+		keys[i] = k
+		i++
+	}
+	sort.Strings(keys)
+	sorted := make([]string, len(params))
+	i = 0
+	for _, k := range keys {
+		sorted[i] = k + "=" + url.QueryEscape(params[k])
+		i++
+	}
+	return strings.Join(sorted, "&")
+}
+
+func Request(method string, path string, params ReqParams) (result string, err error) {
+	client := &http.Client{}
+	sorted := SortParams(params)
+	var req *http.Request
+	if method == POST {
+		req, _ = http.NewRequest(method, path, strings.NewReader(sorted))
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	} else {
+		req, _ = http.NewRequest(method, path+"?"+sorted, strings.NewReader(""))
+	}
+
+	fmt.Println("Client Do......")
+	resp, err := client.Do(req)
+	fmt.Println("resp is", resp)
+	if err != nil {
+		return
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	result = string(body)
+	return
+}
