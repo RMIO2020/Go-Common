704b7cfffdf7d774fd4654aac7435246d22bc2e6
wuyongzhan@rockminer.com <2268769732@qq.com>
2020-07-03 15:54:23 +0800

    [version] -- redis key 增加google发送key

--- helper/cache/redis_key.go
+++ helper/cache/redis_key.go
@@ -1,8 +1,10 @@
 package cache
 
 const (
-	UserToken     = "login_token_"   // 用户-token
-	GoogleKey     = "GG:KEY:"        // 用户-token
-	Transaction   = "Tx:"            // 处理交易 Token
-	CachePayOrder = "CachePayOrder:" // 处理交易 Token
+	UserToken      = "login_token_"         // 用户-token
+	GoogleKey      = "GG:KEY:"              // 用户-token
+	Transaction    = "Tx:"                  // 处理交易 Token
+	CachePayOrder  = "CachePayOrder:"       // 处理交易 Token
+	SetGoogleSms   = "USER_SMS_BINDING_ "   // 设置google发送短信验证码
+	SetGoogleEmail = "USER_EMAIL_BINDING_ " // 设置google发送邮件验证码
 )
