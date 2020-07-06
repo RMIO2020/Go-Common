4149b49f558c87d3b77aeefa4b878a2bb20be011
wuyongzhan@rockminer.com <2268769732@qq.com>
2020-07-06 17:36:13 +0800

    [version] -- 修改common的版本号

--- helper/cache/redis_key.go
+++ helper/cache/redis_key.go
@@ -1,13 +1,13 @@
 package cache
 
 const (
-	UserToken      = "login_token_"     // 用户-token
-	GoogleKey      = "GG:KEY:"          // 用户-token
-	Transaction    = "Tx:"              // 处理交易 Token
-	CachePayOrder  = "CachePayOrder:"   // 处理交易 Token
-	OrderCountDown = "ping:order:down:" //拼团倒计时
-	SetGoogleSms   = "USER_SMS_BINDING_ "   // 设置google发送短信验证码
-	SetGoogleEmail = "USER_EMAIL_BINDING_ " // 设置google发送邮件验证码
+	UserToken      = "login_token_"          // 用户-token
+	GoogleKey      = "GG:KEY:"               // 用户-token
+	Transaction    = "Tx:"                   // 处理交易 Token
+	CachePayOrder  = "CachePayOrder:"        // 处理交易 Token
+	OrderCountDown = "ping:order:down:"      //拼团倒计时
+	SetGoogleSms   = "USER_SMS_BD_GOOGLE_"   // 设置google发送短信验证码
+	SetGoogleEmail = "USER_EMAIL_BD_GOOGLE_" // 设置google发送邮件验证码
 )
 
 /* 获取支付缓存 Key */
