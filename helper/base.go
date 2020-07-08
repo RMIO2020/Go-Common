349a827293b5ba5b3c764b4cd63eb77d23d84894
石姚嘉 <shiyaojia@rockminer.com>
2020-07-08 12:14:33 +0800

    支付单号生成

--- helper/base.go
+++ helper/base.go
@@ -1,72 +1,19 @@
 package helper
 
 import (
+	"crypto/md5"
 	"encoding/base64"
 	"fmt"
+	"github.com/google/uuid"
 	"github.com/skip2/go-qrcode"
-	"math"
 	"math/rand"
 	"os"
-	"strconv"
-	"strings"
 	"time"
 )
 
 func GetProjectRoot() (path string, err error) {
 	path, err = os.Getwd()
 	return
-}
-
-//生成单号
-//sum 最少10位,sum 表示全部单号位数
-func MakeYearDaysRand(sum int, title string) string {
-	// 标头
-	strs := time.Now().Format(title)
-	//一年中的第几天
-	days := strconv.Itoa(GetDaysInYearByThisYear())
-	count := len(days)
-	if count < 3 {
-		//重复字符0
-		days = strings.Repeat("0", 3-count) + days
-	}
-	//组合
-	strs += days
-	//剩余随机数
-	sum = sum - 5
-	if sum < 1 {
-		sum = 5
-	}
-
-	ran := rand.New(rand.NewSource(99))
-	pow := math.Pow(10, float64(sum)) - 1
-	result := strconv.Itoa(ran.Intn(int(pow)))
-	count = len(result)
-	if count < sum {
-		//重复字符0
-		result = strings.Repeat("0", sum-count) + result
-	}
-	//组合
-	strs += result
-	return strs
-}
-
-//年中的第几天
-func GetDaysInYearByThisYear() int {
-	now := time.Now()
-	total := 0
-	arr := []int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
-	y, month, d := now.Date()
-	m := int(month)
-	for i := 0; i < m-1; i++ {
-		total = total + arr[i]
-	}
-	if (y%400 == 0 || (y%4 == 0 && y%100 != 0)) && m > 2 {
-		total = total + d + 1
-
-	} else {
-		total = total + d
-	}
-	return total
 }
 
 func CreateQrCodeBase64(params string) (encoded string, err error) {
@@ -82,4 +29,48 @@ func CreateQrCodeBase64(params string) (encoded string, err error) {
 	}
 	encoded = fmt.Sprintf("data:image/jpg;base64,%v", base64.StdEncoding.EncodeToString(png))
 	return
+}
+
+// MD5 使用md5对数据进行加密
+func MD5(str string) string {
+	md5str := fmt.Sprintf("%x", md5.Sum([]byte(str)))
+	return md5str
+}
+
+// SwitchTimeStampToStr 将传入的时间戳转为时间
+func SwitchTimeStampToStr(timeStamp int64) string {
+	t := time.Unix(timeStamp, 0)
+	return t.Format("2006-01-02 15:04:05")
+}
+
+// Uid 生成uuid
+func Uid() string {
+	return uuid.Must(uuid.NewRandom()).String()
+}
+
+var defaultLetters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+// RandomString 随机字符串
+func RandomString(n int, allowedChars ...[]rune) string {
+	var letters []rune
+	if len(allowedChars) == 0 {
+		letters = defaultLetters
+	} else {
+		letters = allowedChars[0]
+	}
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func CreatePayOrderNo(Sum int, Platform string) string {
+	str := CreateOrderSn(Sum)
+	return Platform + ":" + str
+}
+
+func CreateOrderSn(Sum int) string {
+	return time.Now().Format("20060102150405") + RandomString(Sum)
 }
