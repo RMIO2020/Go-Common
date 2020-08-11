3cb1921dbc9a840068fb6701b40550d578055488
Stone <syj88668@163.com>
2020-08-11 18:53:09 +0800

    rate 发布

--- helper/rate/get.go
+++ helper/rate/get.go
@@ -14,8 +14,10 @@ func CalculationAmountToUSDT(Currency string, Amount float64) (result float64) {
 
 func CalculationAmountUSDTToBTC(Amount float64) (result float64) {
 	rate := GetRateToUSDT("BTC")
-	//result, _ = decimal.NewFromFloat(Amount).Sub(decimal.NewFromFloat(rate)).Float64()
-	result = Amount / rate
+	if rate != 0 {
+		//result, _ = decimal.NewFromFloat(Amount).Sub(decimal.NewFromFloat(rate)).Float64()
+		result = Amount / rate
+	}
 	return
 }
 
@@ -31,24 +33,27 @@ func CalculationAmountToCNY(Currency string, Amount float64) (result float64) {
 
 func GetRateToUSDT(Currency string) (result float64) {
 	Currency = strings.ToUpper(Currency)
+	var err error
 	switch Currency {
 	case "BTC":
-		rVal := Red.Get("BTC-USDT").Val()
-		result, _ = strconv.ParseFloat(rVal, 64)
+		rVal := Red.Get("rate1").Val()
+		result, err = strconv.ParseFloat(rVal, 64)
 	case "ETH":
-		rVal := Red.Get("ETH-USDT").Val()
-		result, _ = strconv.ParseFloat(rVal, 64)
+		rVal := Red.Get("rate2").Val()
+		result, err = strconv.ParseFloat(rVal, 64)
 	case "ZEC":
-		rVal := Red.Get("ZEC-USDT").Val()
-		result, _ = strconv.ParseFloat(rVal, 64)
+		rVal := Red.Get("rate5").Val()
+		result, err = strconv.ParseFloat(rVal, 64)
 	case "CKB":
-		rVal := Red.Get("CKB-USDT").Val()
-		result, _ = strconv.ParseFloat(rVal, 64)
+		result = 0
 	case "USDT":
 		result = 1
 	case "CNY":
 		result = 0.1429
 	default:
+		result = 0
+	}
+	if err != nil {
 		result = 0
 	}
 	return
