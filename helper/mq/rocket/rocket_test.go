0a63150be8f09f94ac2b42e3a367ebefbb927946
IEatLemon <syj88668@163.com>
2020-09-07 17:55:18 +0800

    Aliyun Rocket Mq 接入

--- helper/mq/rocket/rocket_test.go
+++ helper/mq/rocket/rocket_test.go
@@ -2,7 +2,9 @@ package rocket
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"github.com/aliyunmq/mq-http-go-sdk"
 	"github.com/apache/rocketmq-client-go/v2/consumer"
 	"github.com/apache/rocketmq-client-go/v2/primitive"
 	"testing"
@@ -51,4 +53,46 @@ func TestMQ_NewPushConsume(t *testing.T) {
 	}
 	err = c.Start()
 	fmt.Printf("Start err is %+v \n", err)
+}
+
+func TestAliYun(t *testing.T) {
+	// 设置HTTP接入域名（此处以公共云生产环境为例）
+	endpoint := "http://1839080919510286.mqrest.cn-beijing.aliyuncs.com"
+	// AccessKey 阿里云身份验证，在阿里云服务器管理控制台创建
+	accessKey := "LTAI4G1L6gECwdPLLzNcuSzy"
+	// SecretKey 阿里云身份验证，在阿里云服务器管理控制台创建
+	secretKey := "fXLuIlgxsevxxUVfNch9bGj65VilOc"
+	// 所属的 Topic
+	topic := "Test"
+	// Topic所属实例ID，默认实例为空
+	instanceId := "MQ_INST_1839080919510286_BXSQIsss"
+	// Gour Id
+	groupId := "GID_RmTest"
+	// Tag
+	tag := "test"
+
+	InitAliyun(endpoint, accessKey, secretKey, "")
+	Mq := NewAliyun()
+
+	//Mq.InitProducer(instanceId, topic)
+	//for v:=0; v < 10; v++ {
+	//	ret, err := Mq.PushMsg("Test123", tag)
+	//	if err != nil {
+	//		fmt.Printf("err : %+v \n", err)
+	//	}
+	//	fmt.Printf("ret.id : %+v \n", ret)
+	//}
+
+	Mq.InitConsumer(instanceId, topic, groupId, tag)
+	Mq.PullMsg(readMsg)
+}
+
+func readMsg(Data []mq_http_sdk.ConsumeMessageEntry) ([]string, error) {
+	var handles []string
+	for k, v := range Data {
+		fmt.Println("K:", k, "| v body:", v.MessageBody)
+		fmt.Println("v", v)
+		handles = append(handles, v.ReceiptHandle)
+	}
+	return handles, errors.New("read over")
 }
