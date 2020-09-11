f9e897f97a8da93824811fa41a131578d7fc384d
Stone <syj88668@163.com>
2020-09-11 11:32:45 +0800

    消息类型

---
+++ helper/smtp/type.go
@@ -0,0 +1,9 @@
+package smtp
+
+const (
+	Registered = "Registered" // 注册
+	ResetPwd   = "ResetPwd"   // 找回、重置 登录密码
+	SetGoogle  = "SetGoogle"  // 设置 Google
+	BdPhone    = "BdPhone"    // 绑定手机号【已登录的状态下】
+	Recharge   = "Recharge"   // 充值到账【已登录的情况下】
+)
