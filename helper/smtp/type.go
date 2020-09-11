782d287904a1ac1f0d4c9e30e931abed0730ca9e
wuyongzhan@rockminer.com <2268769732@qq.com>
2020-09-11 18:33:58 +0800

    [version] -- 绑定邮箱

--- helper/smtp/type.go
+++ helper/smtp/type.go
@@ -4,6 +4,6 @@ const (
 	Registered = "Registered" // 注册
 	ResetPwd   = "ResetPwd"   // 找回、重置 登录密码
 	SetGoogle  = "SetGoogle"  // 设置 Google
-	BdPhone    = "BdPhone"    // 绑定手机号【已登录的状态下】
+	BdEmail    = "BdEmail"    // 绑定邮箱【已登录的状态下】
 	Recharge   = "Recharge"   // 充值到账【已登录的情况下】
 )
