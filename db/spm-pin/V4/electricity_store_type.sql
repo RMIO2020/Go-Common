d165602c68e9f50f6ae2c8d0de6c4ad70ddf7561
wuyongzhan@rockminer.com <2268769732@qq.com>
2020-09-19 18:51:06 +0800

    [version] -- 预存账单

---
+++ db/spm-pin/V4/electricity_store_type.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `electricity_store_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `day` int(11) NOT NULL DEFAULT '0' COMMENT '天数',
+  `amount` decimal(15,2) NOT NULL DEFAULT '0.00' COMMENT '金额',
+  `create_time` datetime NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '添加时间',
+  `update_time` datetime NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='电费单-预存选择';
