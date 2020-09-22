074ff8acae2f21ac63be2a8fd4194ca415bd44b2
wuyongzhan@rockminer.com <2268769732@qq.com>
2020-09-22 16:59:15 +0800

    [version] -- 预存团类型增加team_id

--- db/spm-pin/V4/electricity_store_type.sql
+++ db/spm-pin/V4/electricity_store_type.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `electricity_store_type` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `team_id` int NOT NULL DEFAULT 0 COMMENT '团id',
   `day` int(11) NOT NULL DEFAULT '0' COMMENT '天数',
   `amount` decimal(15,2) NOT NULL DEFAULT '0.00' COMMENT '金额',
   `create_time` datetime NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '添加时间',
--- db/spm-pin/V4/order.sql
+++ db/spm-pin/V4/order.sql
@@ -2,4 +2,3 @@ ALTER TABLE `order` ADD COLUMN `pay_usdt_amount`  decimal(15,8) NOT NULL DEFAULT
 ALTER TABLE `order` ADD COLUMN `pay_cny_amount`  decimal(15,2) NOT NULL DEFAULT 0 COMMENT '用户支付cny金额（包括预存账单）' AFTER `pay_usdt_amount`;
 ALTER TABLE `order` ADD COLUMN `electricity_order_store_id`  int NOT NULL DEFAULT 0 COMMENT '预存电费单id' AFTER `pay_cny_amount`;
 ALTER TABLE `order` ADD COLUMN `has_request_pay`  enum('NO','YES') NOT NULL DEFAULT 'NO' COMMENT '是否有请求支付' AFTER `remarks`;
-ALTER TABLE `order` ADD COLUMN `pay_url`  varchar(1024) NOT NULL DEFAULT '' COMMENT '支付链接' AFTER `has_request_pay`;
