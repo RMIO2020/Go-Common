ec15e287313d2fe680d4a54661703572e5a65c4f
Stone <syj88668@163.com>
2020-09-29 16:19:39 +0800

    CNY 汇率

--- helper/rate/get.go
+++ helper/rate/get.go
@@ -66,10 +66,11 @@ func GetRateToUSDT(Currency string) (result float64) {
 		result = 1
 	case "CNY":
 		rVal := Red.Get(CNYUSDT).Val()
-		result, err = strconv.ParseFloat(rVal, 64)
+		result, err := strconv.ParseFloat(rVal, 64)
 		if result == 0 || err != nil {
-			result = 1 / CNYTOUSD
+			result = CNYTOUSD
 		}
+		result = 1 / result
 	default:
 		result = 0
 	}
