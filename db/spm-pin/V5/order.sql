eccf4439cb4ebcae7401449abe1af54f3e8cca07
wuyongzhan@rockminer.com <2268769732@qq.com>
2020-10-19 11:54:30 +0800

    [bug] -- 调整

--- db/spm-pin/V5/order.sql
+++ db/spm-pin/V5/order.sql
@@ -2,3 +2,5 @@ ALTER TABLE `order` ADD COLUMN `pay_usdt_amount`  decimal(15,8) NOT NULL DEFAULT
 ALTER TABLE `order` ADD COLUMN `pay_cny_amount`  decimal(15,2) NOT NULL DEFAULT 0 COMMENT '用户支付cny金额（包括预存账单）' AFTER `pay_usdt_amount`;
 ALTER TABLE `order` ADD COLUMN `electricity_order_store_id`  int NOT NULL DEFAULT 0 COMMENT '预存电费单id' AFTER `pay_cny_amount`;
 ALTER TABLE `order` ADD COLUMN `has_request_pay`  enum('NO','YES') NOT NULL DEFAULT 'NO' COMMENT '是否有请求支付' AFTER `remarks`;
+ALTER TABLE `order` MODIFY COLUMN `refund_amount`  decimal(15,8) NOT NULL DEFAULT 0.00 COMMENT '退款金额' AFTER `pay_status`;
+ALTER TABLE `order` ADD COLUMN `pay_amount`  decimal(15,8) NOT NULL DEFAULT 0 COMMENT '实际支付金额' AFTER `pay_cny_amount`;
