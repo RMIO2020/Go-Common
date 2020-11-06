7edd24d534072bc5267c38bf07f237030a5b85b4
wuyongzhan@rockminer.com <2268769732@qq.com>
2020-11-06 12:14:59 +0800

    [version] -- 商品购买的团增加单人成团信息，账单支付增加支付订单号

--- db/spm-pin/V1.6/team.sql
+++ db/spm-pin/V1.6/team.sql
@@ -1,3 +1,7 @@
 ALTER TABLE `team` ADD COLUMN `type`  enum('ROUTINE','SINGLE') NOT NULL DEFAULT 'ROUTINE' COMMENT '类型：routine为常规，single为单人成团' AFTER `team_name`;
 ALTER TABLE `team` ADD COLUMN `source_id`  int NOT NULL DEFAULT 0 COMMENT '来源id（如果是单人成团则是原团id）' AFTER `type`;
 ALTER TABLE `team` ADD COLUMN `single_machine_num`  int NOT NULL DEFAULT 0 COMMENT '单人成团的矿机份数' AFTER `total_num`;
+
+-- 2020.11.06 吴永展 增加单人成团信息
+ALTER TABLE `team` ADD COLUMN `single_order_no`  varchar(256) NOT NULL DEFAULT '' COMMENT '单人成团订单号' AFTER `remarks`,
+ALTER TABLE `team` ADD COLUMN `single_user_id`  varchar(128) NOT NULL DEFAULT '' COMMENT '单人成团用户id' AFTER `single_order_no`;
