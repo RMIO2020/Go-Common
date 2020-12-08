91d39961248099d09624e3c96435005e23e82419
wuyongzhan@rockminer.com <2268769732@qq.com>
2020-12-08 18:30:39 +0800

    [version] -- 首单订单回调

--- helper/mq/rocket/tag.go
+++ helper/mq/rocket/tag.go
@@ -1,10 +1,11 @@
 package rocket
 
 const (
-	Notice       = "notice"
-	OrderPin     = "orderPin"
-	OrderHosting = "orderHosting"
-	Transaction  = "transaction"
-	SystemNotice = "Sys"
-	PayCredit    = "payCredit"
+	Notice          = "notice"
+	OrderPin        = "orderPin"
+	OrderHosting    = "orderHosting"
+	Transaction     = "transaction"
+	SystemNotice    = "Sys"
+	PayCredit       = "payCredit"
+	FirstGoodsOrder = "firstGoodsOrder"
 )
