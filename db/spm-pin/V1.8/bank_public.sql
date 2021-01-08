4b3e6635665eb91fd5d185aa71f8b7c9e238c8a5
wuyongzhan@rockminer.com <2268769732@qq.com>
2021-01-08 11:40:19 +0800

    [version] -- 订单修改状态完成

--- db/spm-pin/V1.8/bank_public.sql
+++ db/spm-pin/V1.8/bank_public.sql
@@ -1,11 +1,10 @@
 CREATE TABLE `bank_public` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `bank_card_username` varchar(128) NOT NULL DEFAULT '' COMMENT '账户名称',
-  `bank_card` varchar(128) NOT NULL DEFAULT '' COMMENT '银行名称',
-  `bank_card_no` varchar(128) NOT NULL DEFAULT '' COMMENT '银行账户（卡号）',
+  `public_bank_card_username` varchar(128) NOT NULL DEFAULT '' COMMENT '账户名称',
+  `public_bank_card` varchar(128) NOT NULL DEFAULT '' COMMENT '银行名称',
+  `public_bank_card_no` varchar(128) NOT NULL DEFAULT '' COMMENT '银行账户（卡号）',
   `status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '状态：1为正常，2为失效',
   `create_time` datetime NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '添加时间',
   `update_time` datetime NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '更新时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='银行卡转账公有信息表';
-
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COMMENT='银行卡转账公有信息表';
