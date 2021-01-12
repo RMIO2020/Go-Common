2ceef4eaa8e669ca0ff986997e006a6e3aa77616
wuyongzhan@rockminer.com <2268769732@qq.com>
2021-01-12 17:54:15 +0800

    [bug] -- 支付待支付的订单

--- db/spm-pin/V1.8/electricity_order_user.sql
+++ db/spm-pin/V1.8/electricity_order_user.sql
@@ -4,3 +4,4 @@ ALTER TABLE `electricity_order_user` ADD COLUMN `bank_card_no`  varchar(128) NOT
 ALTER TABLE `electricity_order_user` ADD COLUMN `bank_card`  varchar(128) NOT NULL DEFAULT '' COMMENT '开户行' AFTER `bank_card_no`;
 ALTER TABLE `electricity_order_user` ADD COLUMN `bank_card_username`  varchar(128) NOT NULL DEFAULT '' COMMENT '银行卡姓名' AFTER `bank_card`;
 ALTER TABLE `electricity_order_user` ADD COLUMN `pay_check_time` datetime NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '银行转账审核时间' AFTER `bank_card_username`;
+ALTER TABLE `electricity_order_user` MODIFY COLUMN `status`  enum('FAIL','SUCCESS','CONFIRMING','WAIT') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'WAIT' COMMENT '状态' AFTER `usdt_rate`;
