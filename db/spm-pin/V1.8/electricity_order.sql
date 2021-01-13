aaaefbb5127244efe30480fc0035806f91f50640
wuyongzhan@rockminer.com <2268769732@qq.com>
2021-01-13 15:37:25 +0800

    [bug] -- 支付待支付的订单

--- db/spm-pin/V1.8/electricity_order.sql
+++ db/spm-pin/V1.8/electricity_order.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `electricity_order` ADD COLUMN `bank_log_id`  int NOT NULL DEFAULT 0 COMMENT '银行卡转账记录id' AFTER `pay_time`;
 ALTER TABLE `electricity_order` MODIFY COLUMN `pay_status`  enum('FAIL','SUCCESS','BEING','CONFIRMING','TIMEOUT','WAIT') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'WAIT' COMMENT '支付状态' AFTER `protocol`;
-
+ALTER TABLE `electricity_order` MODIFY COLUMN `pay_type`  enum('','WECHAT','ALIPAY','BANK','VIRTUAL') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'VIRTUAL' COMMENT '支付方式' AFTER `usdt_rate`;
 
