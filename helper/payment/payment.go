ade52895f1859554b9d67109dd4e85ef494647af
wuyongzhan@rockminer.com <2268769732@qq.com>
2021-01-15 15:54:25 +0800

    [version] -- token

--- helper/payment/payment.go
+++ helper/payment/payment.go
@@ -1,5 +1,24 @@
 package payment
 
-func Get() {
-	return
+// GetEmailPayType 获取邮件的支付方式
+func GetEmailPayType(payType string) string {
+	switch payType {
+	case "WECHAT":
+		return "微信"
+	case "VIRTUAL":
+		return "虚拟币"
+	default:
+		return "其他支付方式"
+	}
+}
+
+func GetEmailCurrencyUnit(currency string) string {
+	switch currency {
+	case "CNY":
+		return "元"
+	case "USDT":
+		return "USDT"
+	default:
+		return "元"
+	}
 }
